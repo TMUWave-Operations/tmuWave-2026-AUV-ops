d3aa5c77352aff1870d35a91ec45ac71b619842f
Rhea Braich <braich5000@gmail.com>
2026-05-26 21:26:09 +0000

    add doc-string example

--- autonomy/mission_state_machine.py
+++ autonomy/mission_state_machine.py
@@ -1,1 +1,22 @@
+# class StateMachine:
 
+# Doc-strings - document what the class or method does
+"""
+EXAMPLE**
+
+ Objective: 
+    - create basic AUV mission state transitions
+
+    Args:
+    - vision data
+    - telemetry data
+    - mission parameters
+
+    Returns:
+    - update the mission state
+    
+    Notes:
+    - expand here
+    """
+
+    # code here 
